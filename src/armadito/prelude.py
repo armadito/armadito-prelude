8ffaf391ddb853319e524a91706d14c85f72f7f0
François Déchelle <fdechelle@teclib.com>
2016-08-08 16:26:57 +0200

    Add partial implementation of Prelude alert
    First working version of detection event sent as alert to Prelude

--- src/armadito/prelude.py
+++ src/armadito/prelude.py
@@ -1,6 +1,28 @@
 import prelude
-from PreludeEasy import ClientEasy
+from PreludeEasy import ClientEasy, IDMEF
+import prelude
 
 class PreludeClient(ClientEasy):
     def __init__(self):
         ClientEasy.__init__(self, "armadito-prelude", 4, "armadito antivirus", "armadito antivirus class", "Teclib'")
+
+    def send_scan_event(self, json_event):
+        idmef = IDMEF()
+
+        # Source
+        #idmef.Set("alert.source(0).node.address(0).address", "127.0.0.1")
+
+        # Target
+        idmef.Set("alert.classification.text", "Threat detected")
+        idmef.Set("alert.target(0).file(0).name", json_event['module_report'].encode('ascii', 'replace'))
+        idmef.Set("alert.target(0).file(0).path", json_event['path'].encode('ascii', 'replace'))
+
+        # Assessment
+        #idmef.Set("alert.assessment.impact.severity", "low")
+        #idmef.Set("alert.assessment.impact.completion", "failed")
+        #idmef.Set("alert.assessment.impact.type", "recon")
+
+        # Additional Data
+        #idmef.Set("alert.additional_data(0).data", '')
+
+        self.SendIDMEF(idmef)        
--- src/armadito/rest.py
+++ src/armadito/rest.py
@@ -2,10 +2,13 @@ import requests
 import json
 
 class ApiClient(object):
-    def __init__(self, port = 8888):
+    def __init__(self, port = 8888, verbose = False):
         self._port = port
         self._api_root = 'http://127.0.0.1:%d/api' % (self._port,)
         self._token = None
+#        self._config = {}
+#        if verbose:
+#            self._config['verbose'] = sys.stderr
 
     def call(self, path, data = None):
         headers = {'User-Agent':'python-requests'}
@@ -13,12 +16,11 @@ class ApiClient(object):
             headers['X-Armadito-Token'] = self._token
         if data is not None:
             headers['Content-Type'] = 'application/json'
-
         if data is not None:
+# error on config            resp = requests.post(self._api_root + path, data = json.dumps(data), headers = headers, config = self._config)
             resp = requests.post(self._api_root + path, data = json.dumps(data), headers = headers)
         else:
             resp = requests.get(self._api_root + path, headers = headers)
-
         return resp.json()
 
     def register(self):
