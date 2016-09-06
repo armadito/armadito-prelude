e030bba7f8ea0f95fe625afb842f6cefdaf68053
François Déchelle <fdechelle@teclib.com>
2016-09-06 15:12:48 +0200

    Move to prelude 3.0.0

--- src/armadito/prelude.py
+++ src/armadito/prelude.py
@@ -1,21 +1,19 @@
 import prelude
-from PreludeEasy import ClientEasy, IDMEF
-import prelude
 
-class PreludeClient(ClientEasy):
+class PreludeClient(prelude.ClientEasy):
     def __init__(self):
-        ClientEasy.__init__(self, "armadito-prelude", 4, "armadito antivirus", "armadito antivirus class", "Teclib'")
+        prelude.ClientEasy.__init__(self, "armadito-prelude", 4, "armadito antivirus", "armadito antivirus class", "Teclib'")
 
     def send_scan_event(self, json_event):
-        idmef = IDMEF()
+        idmef = prelude.IDMEF()
 
         # Source
         #idmef.Set("alert.source(0).node.address(0).address", "127.0.0.1")
 
         # Target
-        idmef.Set("alert.classification.text", "Threat detected")
-        idmef.Set("alert.target(0).file(0).name", json_event['module_report'].encode('ascii', 'replace'))
-        idmef.Set("alert.target(0).file(0).path", json_event['path'].encode('ascii', 'replace'))
+        idmef.set("alert.classification.text", "Threat detected")
+        idmef.set("alert.target(0).file(0).name", json_event['module_report'].encode('ascii', 'replace'))
+        idmef.set("alert.target(0).file(0).path", json_event['path'].encode('ascii', 'replace'))
 
         # Assessment
         #idmef.Set("alert.assessment.impact.severity", "low")
@@ -25,4 +23,4 @@ class PreludeClient(ClientEasy):
         # Additional Data
         #idmef.Set("alert.additional_data(0).data", '')
 
-        self.SendIDMEF(idmef)        
+        self.sendIDMEF(idmef)        
