1f0138aa497b188ae35ea8bc533f49120dc65e77
Valentin Hamon <vhamon@teclib.com>
2016-09-15 11:19:20 +0200

    Add basic XML parser using XML::Bare

---
+++ perl/lib/Armadito/Prelude/XML/Parser.pm
@@ -0,0 +1,66 @@
+package Armadito::Prelude::XML::Parser;
+
+use strict;
+use warnings;
+use English qw(-no_match_vars);
+use UNIVERSAL::require;
+use XML::Bare;
+
+require Exporter;
+
+sub new {
+	my ( $class, %params ) = @_;
+
+	warn "Empty xml virus alert content." if !$params{text};
+
+	my $self = { 
+	 parser => new XML::Bare(text => $params{text}),
+	 xmlparsed => ""
+	};
+
+	bless $self, $class;
+	return $self;
+}
+
+
+sub run {	
+	my ( $self, %params ) = @_;
+
+	eval {$self->{xmlparsed} = $self->{parser}->parse()};
+	if ($@) {
+	  warn "Error when parsing XML alert ".$@;
+	}	
+
+	return $self;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Armadito::Prelude::XML::Parser - simple XML parser class using XML::Bare module.
+
+=head1 DESCRIPTION
+
+Given plain text content is parsed with XML::Bare.
+
+=head1 METHODS
+
+=head2 $parser->new(%params)
+
+New parser instanciation.
+
+=over
+
+=item I<text>
+
+Plain text input to be parsed with XML::Bare.
+
+=back
+
+=head2 $parser->run()
+
+Run parser on input given.
+
