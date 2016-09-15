9af613744b10d03337c20bb2c838437b4d6eab9c
Valentin Hamon <vhamon@teclib.com>
2016-09-15 10:45:11 +0200

    Now arg is only INSTALL_BASE due to a conflict between INSTALL_BASE and PREFIX variables

--- perl/do_all.sh
+++ perl/do_all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#set -e
+set -e
 
 PRELUDE_PREFIX=~/prelude/install/
 
 tidyall -a
-perl Makefile.PL PREFIX=$PRELUDE_PREFIX
+perl Makefile.PL INSTALL_BASE=$PRELUDE_PREFIX
 make
 LD_LIBRARY_PATH=$PRELUDE_PREFIX/lib PERL5LIB=$PRELUDE_PREFIX/lib/x86_64-linux-gnu/perl/5.22.1/ make test
 make install
