2e0a366489c838865ea12d5c702e3eed84731e9f
Valentin Hamon <vhamon@teclib.com>
2016-09-19 11:57:45 +0200

    Add send alert to prelude SIEM in detectionEvent

--- perl/lib/Armadito/Prelude/HTTP/Client/ArmaditoAV/Event/DetectionEvent.pm
+++ perl/lib/Armadito/Prelude/HTTP/Client/ArmaditoAV/Event/DetectionEvent.pm
@@ -3,11 +3,51 @@ package Armadito::Prelude::HTTP::Client::ArmaditoAV::Event::DetectionEvent;
 use strict;
 use warnings;
 use base 'Armadito::Prelude::HTTP::Client::ArmaditoAV::Event';
+use Armadito::Prelude::IDMEF;
 
 sub new {
 	my ( $class, %params ) = @_;
 
 	my $self = $class->SUPER::new(%params);
+	$self->{idmef} = Armadito::Prelude::IDMEF->new();
+
+	return $self;
+}
+
+sub _sendToPrelude {
+	my ( $self, $message ) = @_;
+
+	$self->{prelude_client}->{client}->sendIDMEF( $self->{idmef}->{obj} );
+
+	return $self;
+}
+
+sub _setIDMEF {
+	my ( $self, $message ) = @_;
+
+	$self->{idmef}->setAnalyzer();
+	$self->{idmef}->setClassification( text => "Virus found: " . $self->{jobj}->{module_report} );
+
+	$self->{idmef}->setAssessment(
+		impact_severity    => "high",
+		impact_type        => "file",
+		impact_completion  => "succeeded",
+		impact_description => "Virus detected by Armadito AV (on-demand scan)"
+	);
+
+	$self->{idmef}->setAdditionalData(
+		i       => 0,
+		type    => "string",
+		meaning => "File location",
+		data    => $self->{jobj}->{path}
+	);
+
+	$self->{idmef}->setAdditionalData(
+		i       => 1,
+		type    => "string",
+		meaning => "Virus name",
+		data    => $self->{jobj}->{module_report}
+	);
 
 	return $self;
 }
@@ -15,6 +55,8 @@ sub new {
 sub run {
 	my ( $self, %params ) = @_;
 
+	$self->_setIDMEF();
+	$self->_sendToPrelude();
 	return $self;
 }
 1;
